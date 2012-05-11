94dda8f310eef27f5dc05f31bfd9a487a4b9f264
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-05-10 20:10:03 -0500

    Adding Problem #3 Solution #2

--- muzzler/problem/p3.py
+++ muzzler/problem/p3.py
@@ -1,5 +1,7 @@
 import math
 
+number = 600851475143
+
 def median(n):
     return ((n-1) // 2) + 1
 
@@ -21,9 +23,9 @@ def is_prime_1(n):
         return n == 1 or len(get_factors_1(n)) == 2
 
 def solution_1():
+    """ My initial stab at the problem """
     global answer
 
-    number = 600851475143
     factors = get_factors_1(number)
     factors.sort(reverse=True)
 
@@ -32,5 +34,35 @@ def solution_1():
             answer = factor
             return answer
 
+def get_factors_2(n):
+    limit = n
+    factors = []
+    i = 1
+    while i < limit:
+        q, r = divmod(n, i)
+        if r == 0:
+            factors += [ i, q ]
+        limit = (n // i) + 1
+        i += 1
+    return factors
+
+def is_prime_2(n):
+    if (n % 2) == 0:
+        return False
+    else:
+        return n == 1 or len(get_factors_1(n)) == 2
+
+def solution_2():
+    """ Same as solution_1, but use divmod() in get_factors_2() """
+    global answer
+
+    factors = get_factors_2(number)
+    factors.sort(reverse=True)
+
+    for factor in factors:
+        if is_prime_2(factor):
+            answer = factor
+            return answer
+
 answer = None
-solutions = [ solution_1 ]
+solutions = [ solution_1, solution_2 ]
